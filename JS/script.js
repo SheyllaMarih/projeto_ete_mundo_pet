6d88686ffe29764525caecc8a1a430ec12f6254b
SheyllaMarih <sheyllamaria528@gmail.com>
2025-05-08 20:49:29 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+function enviarFormulario(event) {
+    event.preventDefault();
+    const nome = document.getElementById(`nomeContato`).value;
+    const email = document.getElementById(`emailContato`).value;
+    const telefone = document.getElementById(`telefoneContato`).value;
+
+    const mensagem =
+    Dados enviados com sucesso!<br>
+    Nome: ${nome}<br>
+    Email: ${email}<br>
+    Telefone: ${telefone}<br>
+
+    document.getElementById(`mensagem-confirmacao`).innerHTML = mensagem;
+}
